5975acf33e41f2ac20cafa613062a1b6da103e8c
Shekhar <shekharphulsare@gmail.com>
2025-09-10 13:58:13 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,5 @@
+let name = "shekhar" // string dataType
+let id = 121 // number dataType
+let isLoggedIn = true // boolean
+let temp = null // null
+let salary; // undefined
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,4 +1,18 @@
-const accoutId = 231;
+const accountId = 231;
 let accountEmail = 'Shekhar@gmail.com';
 var accountPassword = '1234';
 accountCity = 'Solapur';
+
+// accountId = 543; // not allowed
+accountEmail = 'Shri@gmail.com';
+accountPassword = '321';
+accountCity = "Kolhapur";
+
+console.table([accountId , accountEmail , accountPassword , accountCity]);
+
+var name = 'shekhar';
+{
+    var name = 'shri';
+    console.log(name);
+}
+console.log(name);
